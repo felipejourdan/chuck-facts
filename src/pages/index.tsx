026c6410b4942667df9138820c6783f6110a7ccd
felipejourdan <felipejourdan@MacBook-Air.Home>
2022-05-19 11:08:45 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from 'next'
+import { useEffect, useState } from 'react'
+
+interface factProps {}
+
+const Home: NextPage = () => {
+  const [facts, setFacts] = useState([''])
+  const [factToShow, setFactToShow] = useState('')
+  const [factCount, setFactCount] = useState(0)
+  const [triggerCallApi, setTriggerCallApi] = useState(false)
+
+  useEffect(() => {
+    const axios = require('axios')
+    const blackList = ['fuck', 'ass', 'bitch', 'jackass']
+
+    async function getFacts() {
+      const factsList = []
+      let i
+
+      for (i = 0; i < 10; i++) {
+        const { data } = await axios.get(
+          'https://api.chucknorris.io/jokes/random'
+        )
+        const factSplit = data.value.toLowerCase().split(' ' || ',' || '.')
+        const check = factSplit.some((word: string) => blackList.includes(word))
+        factsList.push(data.value)
+      }
+      console.log('fez call api')
+      setFacts(factsList)
+    }
+
+    getFacts()
+  }, [triggerCallApi])
+
+  function handleShowFact() {
+    setFactToShow(facts[factCount])
+    setFactCount(factCount + 1)
+  }
+
+  if (factCount === 10) {
+    setTriggerCallApi(!triggerCallApi)
+    setFactCount(0)
+  }
+
+  function handleClick() {
+    console.log(facts)
+  }
+
+  return (
+    <>
+      <h1>ChuckFacts</h1>
+      <p>{factToShow}</p>
+      <button onClick={handleShowFact}>Generate</button>
+      <button onClick={handleClick}>ver facts</button>
+    </>
+  )
+}
+
+export default Home
